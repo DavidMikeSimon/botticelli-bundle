1e5e0976819fde9a0a5f4190d6f0d6aa597dbda8
David Simon <dsimon@americancouncils.org>
2013-01-16 16:25:31 -0500

    Dealing with weird doctrine errors on kernel boot

--- ShellStartup.php
+++ ShellStartup.php
@@ -7,8 +7,25 @@ require_once $basedir . '/app/AppKernel.php';
 unset($basedir);
 
 $kernel = new AppKernel('dev', false);
-$kernel->boot();
+
+$retries = 3;
+while ($retries > 0) {
+    try {
+        $kernel->boot();
+        break;
+    } catch (Exception $e) {
+        // Ignore it, kernel sometimes fails on first execution
+        // FIXME This is a silly hack
+    }
+    --$retries;
+}
+
+if ($retries == 0) {
+    throw new \Exception("Unable to boot Symfony kernel");
+}
+
 $container = $kernel->getContainer();
+
 unset($kernel);
 
 print("Welcome to Botticelli PHP Shell\n");
